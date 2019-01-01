713ff6865926f3cb3daf2a8aa7307377b4b76a88
Waqar Aqeel <waqeel@cs.duke.edu>
2018-12-31 22:10:14 -0500

    all pages ready

--- compile.py
+++ compile.py
@@ -67,10 +67,46 @@ if __name__ == "__main__":
         "research.html": "Research",
         "teaching.html": "Teaching",
         "publications.html": "Publications",
-        "contact.html": "Contact"
+        "#contact": "Contact",
     }
     sb = SourceBundle(templates=["index.html"])
     sb.build(nav_entries=nav_entries, active_page="Home")
 
+    courses = [
+        "18/FA-INFO-601-04 Foundations of Information",
+        "19/SP-INFO-601-02 Foundations of Information",
+        "18/FA-INFO-673-01 Literacy & Instruction",
+        "19/SP-INFO-697-05 Special Topics",
+    ]
     sb = SourceBundle(templates=["teaching.html"])
-    sb.build(nav_entries=nav_entries, active_page="Teaching")
+    sb.build(nav_entries=nav_entries, active_page="Teaching", courses=courses)
+
+    publications = [
+        {
+            "authors": "Bowler, L., Oh, J.S., & He, D.",
+            "year": 2015,
+            "title": "Teen Health Information Behavior and Social Q & A: A Study to Investigate Teens’ Assessments of the Accuracy, Credibility, and Reliability of Health Information about Eating Disorders",
+            "link": "/static/sample.pdf",
+            "venue": "Yahoo! Answers. ALISE 2015. January 27-30, 2015",
+            "location": "Chicago, IL, USA",
+        }
+    ]
+    publications = publications * 10
+
+    sb = SourceBundle(templates=["publications.html"])
+    sb.build(
+        nav_entries=nav_entries, active_page="Publications", publications=publications
+    )
+
+    projects = [
+        {
+            "title": "Project Title",
+            "img": "teaching-head.png",
+            "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique nunc in odio volutpat accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. In nisi erat, tristique vitae ipsum malesuada, aliquet posuere dui. Suspendisse sed sapien eget sem tempus maximus. Duis in nisl semper, placerat quam aliquet, gravida libero. Morbi sit amet placerat orci. Quisque tincidunt sed arcu nec laoreet. Morbi gravida sem lectus. Sed tincidunt, mi quis sagittis consequat, quam lacus egestas dolor, vitae tempor quam nulla vel ipsum. Aenean volutpat dictum ligula, et ultricies ligula tempus sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam neque turpis, cursus sed massa et, accumsan imperdiet risus. Vivamus hendrerit leo ultrices tortor pretium, imperdiet consequat erat suscipit. Maecenas et odio velit.",
+        }
+    ]
+    projects = projects * 10
+
+    sb = SourceBundle(templates=["research.html"])
+    sb.build(nav_entries=nav_entries, active_page="Research", projects=projects)
+
