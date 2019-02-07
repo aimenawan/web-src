fc53f6b323a26a4300ef1c2b41342db5e320388e
Waqar Aqeel <waqeel@cs.duke.edu>
2019-02-06 22:21:07 -0500

    last changes

---
+++ courses.py
@@ -0,0 +1,19 @@
+courses = [
+    {
+        "name": "INFO 601 &mdash; Foundations of Information",
+        "desc": "This foundational course focuses on the intersection of people, information, and technology and the theoretical and conceptual foundations of the information field. Students will be introduced to ideas and concepts that will inform future specializations in their course of study and provide them with concrete strategies for ongoing professional growth and development in their area of interest.",
+    },
+    {
+        "name": "INFO 673 &mdash; Literacy and Instruction",
+        "desc": "This course focuses on teaching and learning in library settings. Examines literacy theories, including critical literacy, digital literacy, media literacy, information literacy and transliteracy. Explores the implementation of literacy-related programs in public libraries, academic libraries, archives, museums and other cultural institutions with a focus on assessing patron needs and using culturally relevant pedagogy.",
+    },
+    {
+        "name": "INFO 697 &mdash; Special Topics: Children & Teens as Users of Technology",
+        "desc": "This course investigates digital technology in young peoples' lives.  The goals for this course are for students to be able to discuss in a critical manner the key issues and current research surrounding technology in the lives of children and youth (birth to 18 years) and to apply their learning to the design of digital experiences for young people.",
+    },
+    {
+        "name": "INFO 697 &mdash; Special Topics: Programming, Services, and Resources for Early Childhood",
+        "desc": "The course explores the specialized skills and knowledge needed to design and develop library programs, services, and resources for infants and young children (0-8 years). The course will follow the literacy continuum, from language development through to independent reading and writing, with a special focus on the first three years of life, early literacy, and the role of family and community. The course will interest students who want to work with young people in different contexts and need specialized knowledge of early childhood, the social dynamics of families, and the early education environment.",
+    },
+]
+
---
+++ projects.py
@@ -0,0 +1,36 @@
+projects = [
+    {
+        "title": "Exploring Data Worlds at the Public Library: A Youth Data Literacy Project",
+        "img": "img/project1.png",
+        "desc": "The <i>Exploring Data Worlds at the Public Library</i> research project is supported by a grant from the Institute for Museum and Library Services. The project explores youth data literacy in the context of youth services in maker spaces at the public library. The goals of the <i>Exploring Data Worlds</i> project are threefold: 1) to generate social science observations about young peoples’ interactions with data, including their self-awareness as data subjects, 2) to discover the unique data literacy needs of digital youth in terms of their critical and civic participation in a data driven society, and 3) develop strategies for training youth librarians in order to provide them with the skills to empower young people. See recent publications for details about this project, Chi, Y., Jeng, W., Acker, A., & Bowler, L., 2018; Acker, A. & Bowler, L., 2018; Bowler, L., Acker, A., Jeng, W. & Chi, Y., 2017; Acker, A. & Bowler, L., 2017. A collaboration with Amelia Acker (UT Austin)",
+        "footer": 'Project web site: <a href="https://www.youthdataliteracy.info/">https://www.youthdataliteracy.info/</a>',
+        "link": "https://www.youthdataliteracy.info/",
+    },
+    {
+        "title": '"Mindful Making": Critical computing and informal STEM learning in Maker Spaces',
+        "img": "img/project2.png",
+        "desc": "This research program, supported through grants from the Sprout Foundation and the Association for Library and Information Science, investigates critical making, self-awareness, and the practice of self-critique in library-based maker spaces. See Bowler, 2016; Bowler, L. & Champagne, R., 2016, for more details.",
+        "footer": 'Download a <i>Mindful Maker Questions</i> poster. It highlights some critical questions that mindful makers can ask themselves: <a href="http://www.mindfulmakerquestions.info/">http://www.mindfulmakerquestions.info</a>',
+        "link": "http://www.mindfulmakerquestions.info/",
+    },
+    {
+        "title": "Remake Making: Understanding Adoption and Adaptation of Facilitated Making in Libraries",
+        "img": "",
+        "desc": "The study explores professional development and community-based, facilitation models in support of informal STEM learning at the public library. The project is supported by a grant from the National Science Foundation. A collaboration with principal investigator Tom Akiva (University of Pittsburgh), Kevin Crowley (University of Pittsburgh), and Peter Wardrip (University Wisconsin-Madison).",
+        "footer": 'Explore facilitation methods with the <i>Simple Interactions</i> tool (Developed by Junelei Li (Harvard), Tom Akiva (University of Pittsburgh) and the team at the Fred Rogers Center: <a href="https://www.simpleinteractions.org/">https://www.simpleinteractions.org</a>',
+        "link": "https://www.simpleinteractions.org/ ",
+    },
+    {
+        "title": '"Think Before You Click!" Designing Social Media Spaces that Afford Moments of Reflection<br>(Study into Cyberbullying)',
+        "img": "img/project4.png",
+        "desc": "This project looked at mean and cruel online behavior through the lens of design, with the goal of developing positive technologies for youth. Visual narrative inquiry – a storytelling method – was used in the early stages of this project to explore the relationship between reflection (the metacognitive piece), values, and interaction design. Seven emergent design themes were evident in the participants’ design recommendations and these design themes were then associated with a values-framework (Cheng & Fleishman, 2010). For more information, see Bowler, Knobel & Mattern, 2015; Fan, M., Liyue, Y. & Bowler, L., 2016.",
+        "link": "",
+    },
+    {
+        "title": "Social Q&A and Teen Health",
+        "img": "",
+        "desc": "This project began with the basic question, <i>How do teens use social media to answer their health questions?</i> Supported through a grant from OCLC, the project looked at a health topic that is relevant to teens – eating disorders – in order to model the interactions between questioners and askers in the social Q&A space of <i>Yahoo! Answers</i>, with the aim of contributing to the greater understanding of youth information behavior, health information literacy, and the role of social Q&A and affect in the provision of health information to young people. See Bowler et al, 2012, 2013; Oh et al, 2013, for more information. A collaboration with Daqing He (University of Pittsburgh), Jung Sun Oh (University of Pittsburgh).",
+        "link": "",
+    },
+]
+
